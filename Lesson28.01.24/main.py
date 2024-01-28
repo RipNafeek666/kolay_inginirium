268d7fbf0f707568633c6d0729ac5e5b31fe41b9
lolkile <125196044+lolkile@users.noreply.github.com>
2024-01-28 17:42:43 +0300

    DZ

---
+++ Lesson28.01.24/main.py
@@ -0,0 +1,47 @@
+import pygame
+pygame.init()
+width = 500
+height = 500
+win = pygame.display.set_mode((width,height))
+
+cx = width / 2
+cy = height / 2
+
+colorCirc = (255, 255, 0)
+
+xCirc = width / 2
+yCirc = height / 2
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        xCirc -= 1
+    elif keys[pygame.K_RIGHT]:
+        xCirc += 1
+    elif keys[pygame.K_UP]:
+        yCirc -= 1
+    elif keys[pygame.K_DOWN]:
+        yCirc += 1
+    else:
+        xCirc = 250
+        yCirc = 250
+
+    if xCirc > xc + 150:
+        colorCirc = (255, 0, 0)
+    if  xCirc < cx - 150:
+        colorCirc = (255, 0, 0)
+    if yCirc > cy + 150:
+        colorCirc = (255, 0, 0)
+    if yCirc < cy - 150:
+        colorCirc = (255, 0, 0)
+    else:
+        colorCirc = (255, 255, 0)
+
+    win.fill((255,255,255))
+    pygame.draw.circle(win, (colorCirc), (xCirc, yCirc), 30)
+    pygame.display.update()
+    pygame.time.delay(10)

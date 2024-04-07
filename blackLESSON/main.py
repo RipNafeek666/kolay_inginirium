3043b893f1074535068d1f688ed35cb66f70ca18
lolkile <156321265+RipNafeek666@users.noreply.github.com>
2024-04-07 17:41:45 +0300

    ,fg,hlh

---
+++ blackLESSON/main.py
@@ -0,0 +1,58 @@
+import sqlite3 as sql
+
+con = sql.connect('shcool.sqlite')
+cur = con.cursor()
+
+que_create = '''
+CREATE TABLE IF NOT EXISTS class (
+    id INTEGER PRIMARY KEY, 
+    name TEXT,
+    surname TEXT,
+    mark INTEGER
+)
+'''
+
+cur.execute(que_create)
+con.commit()
+
+que_insert = '''
+INSERT INTO class (name, surname, mark) VALUES
+    ('Василий', 'Пупкин', 3),
+    ('Дунис', 'Синицын', 4),
+    ('Ангелина', 'Соколова', 5),
+    ('Саша', 'Петров', 2)
+'''
+
+#cur.execute(que_insert)
+#con.commit()
+
+que_select = '''
+SELECT * FROM class
+'''
+
+result = cur.execute(que_select)
+data = result.fetchall()
+for line in data:
+    print(line)
+
+que_zadanie1 = '''
+SELECT name FROM class WHERE mark = 5
+'''
+
+result = cur.execute(que_zadanie1)
+data = result.fetchall()
+for line in data:
+    print(line)
+
+que_zadanie2 = '''
+SELECT name FROM class WHERE mark = 3
+'''
+
+result = cur.execute(que_zadanie2)
+data = result.fetchall()
+for line in data:
+    print(line)
+
+
+
+con.close()
